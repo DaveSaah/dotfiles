2fd616aaed4cd3fa6e49ba8e40159a8ef5d47ac1
David Saah <dasorange.hope@gmail.com>
2024-01-23 20:59:50 +0000

    add neovim config

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  { "folke/tokyonight.nvim", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "echasnovski/mini.starter", enabled = false },
+  { "navarasu/onedark.nvim", enabled = false },
+}
